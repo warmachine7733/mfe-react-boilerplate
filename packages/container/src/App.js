83895b91857fbbc2d46b3f4642e0f41e7bb2e09a
Prateek Jena <prateekjena7733@gmail.com>
2023-01-09 20:40:27 +0530

    header component added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 export default () => {
   return (
-    <>
-      <h1>hi there from container!!</h1>
-      <hr />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
