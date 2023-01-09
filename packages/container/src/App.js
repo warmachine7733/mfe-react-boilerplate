d77e7f30e243854341b7a571f519f35ef8f68ef7
Prateek Jena <prateekjena7733@gmail.com>
2023-01-09 21:21:07 +0530

    randomize prod css classes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
